92815159af54bdd87d1ebe3b25a458df73b22954
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-02 00:17:43 -0600

    adjustments continue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path")
 var logger = require("morgan");
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -10,16 +10,16 @@ var PORT = process.env.PORT || 3000;
 
 var db = require("./models/model");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost/news_stories", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/news_stories", { useNewUrlParser: true });
 
 // db.User.create()
 
