8aaa81caa44b129ec46a72ed39a74a8fe9aa1a75
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-02 03:09:01 -0600

    commenting out excess code

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ var logger = require("morgan");
 
 var PORT = process.env.PORT || 3000;
 
-var db = require("./models/model");
+// var db = require("./models/model");
 
 // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // mongoose.connect(MONGODB_URI);
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
